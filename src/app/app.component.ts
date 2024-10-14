9ba0e18029dbf4e91b357679fb315f6efffafc23
Jhonny <jhonnycgarcia@gmail.com>
2024-10-14 11:45:11 -0500

    Instalar PrimeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes-app';
+
+  constructor(
+    private primengConfig: PrimeNGConfig
+  ) {
+    this.primengConfig.ripple = true;
+  }
 }
