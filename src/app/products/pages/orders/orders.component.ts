44809346df487b8e13d4711be39a8570dd11d9c7
Jhonny <jhonnycgarcia@gmail.com>
2024-10-14 19:23:49 -0500

    Continuación de la aplicación

---
+++ src/app/products/pages/orders/orders.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'products-orders',
+  templateUrl: './orders.component.html',
+  styles: ``
+})
+export class OrdersComponent {
+
+}
